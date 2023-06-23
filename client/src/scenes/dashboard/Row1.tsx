1cf557790c2df7d0da2d20ccb6ab1133f29b3ee7
Vigneshk5 <vigneshpatali5@gmail.com>
2023-06-23 17:43:46 +0530

    redux setup and config

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,9 +1,11 @@
 import DashboardBox from "@/components/DashboardBox";
+import { useGetKpisQuery } from "@/state/api";
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Props = {};
 
 const Row1 = (props: Props) => {
+  const { data } = useGetKpisQuery();
   return (
     <>
       <DashboardBox gridArea="a"></DashboardBox>
